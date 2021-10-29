f94e993c36a4bd4c0286316e280c7392a56c580c
saeinlee <gitsaein@gmail.com>
2021-10-29 12:17:02 +0900

    redis junit test

--- src/test/java/com/saeinlee/oauth_client/OauthClientApplicationTests.java
+++ src/test/java/com/saeinlee/oauth_client/OauthClientApplicationTests.java
@@ -1,5 +1,9 @@
 package com.saeinlee.oauth_client;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,10 +16,15 @@ class OauthClientApplicationTests {
 
     @Test
     void contextLoads() {
-    	Iterable<Person> ps = repository.findAll();
-    	repository.save(Person.builder()
+    	Person p = Person.builder()
     			.age(12)
-    			.id("test").name("name").build());
+    			.id("test").name("name").build();
+    	repository.save(p);
+
+    	Optional<Person> ps = repository.findById(p.getId());
+    	assertEquals(ps.get().getId(), p.getId());
+    	repository.deleteById(p.getId());
+    			
     }
 
 }
